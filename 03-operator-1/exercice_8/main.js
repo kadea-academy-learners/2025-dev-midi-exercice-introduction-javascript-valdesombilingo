39b21121e5b6f8810ae2bf6eb395505f7c289279
cherubin <cmklucas99@gmail.com>
2025-05-15 15:44:03 +0100

    ex6,7,8

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,11 +1,12 @@
-let salaireMensuel,
-  loyer,
-  nourriture,
-  transport,
-  autresDepenses,
-  totalDepenses,
-  reste;
-// Ecrivez votre code ici
+let salaireMensuel = 500;
+let loyer = salaireMensuel * 0.30;
+let nourriture = salaireMensuel * 0.20;
+let transport = salaireMensuel * 0.10;
+let autresDepenses = 50 + 25;
+
+let totalDepenses = loyer + nourriture + transport + autresDepenses;
+
+let reste = salaireMensuel - totalDepenses;
 
 // Ne pas modifier le code ci-dessous
 module.exports = {
