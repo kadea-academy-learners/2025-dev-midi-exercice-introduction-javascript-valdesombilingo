584632647a1fea6c5becc097374f7cb9f3ee5cd7
cherubin <cmklucas99@gmail.com>
2025-05-15 17:21:53 +0100

    rajout de la console

--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -6,14 +6,16 @@ let terrains = 40000000;
 let liquidites = 20000000;
 let totalHeritage = maison + terrains + liquidites;
 
-
+// DEUX Groupes
 let partEnfants = totalHeritage * 0.75;
 let partAutres = totalHeritage * 0.25;
 
+// Enfants
 let partParEnfant = partEnfants / 3;
 paul = partParEnfant;
 marie = partParEnfant;
 
+//Les autres 
 eric = partParEnfant / 2;
 clair = partParEnfant / 2;
 
@@ -22,6 +24,15 @@ let partParAutre = partAutres / 3;
 madameMukuna = partParAutre;
 joseph = partParAutre;
 sarah = partParAutre;
+
+// Affichage
+console.log("Paul :", paul, "CDF");
+console.log("Marie :", marie, "CDF");
+console.log("Éric :", eric, "CDF");
+console.log("Claire :", clair, "CDF");
+console.log("Madame MUKUNA :", madameMukuna, "CDF");
+console.log("Joseph :", joseph, "CDF");
+console.log("Sarah :", sarah, "CDF");
 
 // Ne pas modifier le code ci-dessous
 module.exports = {
