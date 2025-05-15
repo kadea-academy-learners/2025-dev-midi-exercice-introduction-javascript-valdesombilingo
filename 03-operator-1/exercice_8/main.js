df9d561f034affb36869bb6797c5a3c44e672eca
penielmab <makuntimapeniel80@gmail.com>
2025-05-15 19:50:55 +0100

    fin du travail

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,11 +1,12 @@
-let salaireMensuel,
-  loyer,
-  nourriture,
-  transport,
-  autresDepenses,
-  totalDepenses,
-  reste;
+//let salaireMensuel, loyer, nourriture, transport, autresDepenses, totalDepenses, reste;
 // Ecrivez votre code ici
+let salaireMensuel = 500;
+let loyer = salaireMensuel*30/100;
+let nourriture = salaireMensuel*20/100;
+let transport = salaireMensuel*10/100;
+let autresDepenses = 50+25;
+let totalDepenses = loyer + nourriture + transport + autresDepenses;
+let reste = salaireMensuel-totalDepenses;
 
 // Ne pas modifier le code ci-dessous
 module.exports = {
