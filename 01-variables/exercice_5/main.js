7ea8d57fcf62fca79fa6acbc47ecb4b7ad5fbc56
Valdes OMBILINGO <smilogod@gmail.com>
2025-05-16 10:21:27 +0100

    Exercices Introduction à Javascript terminés et tests effectués sans erreurs

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -4,7 +4,9 @@ let celsius;
 // Conversion Celsius à Fahrenheit
 function celsiusToFahrenheit(celsius) {
   // ecrire le code de conversion ici
-  let fahrenheit;
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  console.log(fahrenheit);
+
   // la ligne suivante ne doit pas être modifiée
   return fahrenheit;
 }
--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,6 +1,22 @@
 let paul, marie, eric, clair, madameMukuna, joseph, sarah;
 // Ecrivez votre code ici
+let maison = 60_000_000,
+  terrains = 40_000_000,
+  liquidites = 20_000_000,
+  totalPatrimoine = maison + terrains + liquidites,
+  partCategorie1 = totalPatrimoine * 0.75,
+  partCategorie2 = totalPatrimoine * 0.25,
+  partEnfant = partCategorie1 / 3,
+  partEnfantAlain = partEnfant / 2,
+  partConjointOuFrere = partCategorie2 / 3;
 
+(paul = partEnfant),
+  (marie = partEnfant),
+  (eric = partEnfantAlain),
+  (clair = partEnfantAlain),
+  (madameMukuna = partConjointOuFrere),
+  (joseph = partConjointOuFrere),
+  (sarah = partConjointOuFrere);
 // Ne pas modifier le code ci-dessous
 module.exports = {
   paul,
