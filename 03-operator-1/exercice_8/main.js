2c1c293488dadeb6e20b7cf65998d26f6f2ec010
moise kianga <mkianga421630@gmail.com>
2025-05-19 00:20:24 +0100

    all assignments done

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -2,10 +2,16 @@ let salaireMensuel,
   loyer,
   nourriture,
   transport,
-  autresDepenses,
+  autresDepenses = 50,
   totalDepenses,
   reste;
 // Ecrivez votre code ici
+salaireMensuel = 500;
+loyer = 500*30/100;
+nourriture = salaireMensuel*20/100;
+transport = salaireMensuel*10/100;
+autresDepenses += 25;
+totalDepenses = loyer-nourriture-transport-autresDepenses;
 
 // Ne pas modifier le code ci-dessous
 module.exports = {
--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,4 +1,21 @@
 let paul, marie, eric, clair, madameMukuna, joseph, sarah;
+let maison = 60000000;
+let terrains = 40000000;
+let liquidités = 20000000; 
+let heritage = maison + terrains + liquidités;
+// let trentepourcent = heritage*25/100;
+let douzepointcinqpourcent = vingtcinqpourcentpourcent/2
+let vingtcinqpourcent = heritage*25/100;
+paul = vingtcinqpourcentpourcent;
+marie = vingtcinqpourcentpourcent;
+madameMukuna = vingtcinqpourcent/3;
+joseph = vingtcinqpourcent/3;
+sarah = vingtcinqpourcent/3
+
+//le 75% de l'heritage est 90,000,000 CDF
+//les enfants ont à chacun 30,000,000 CDF
+eric = douzepointcinqpourcent;
+clair = douzepointcinqpourcent;
 // Ecrivez votre code ici
 
 // Ne pas modifier le code ci-dessous
