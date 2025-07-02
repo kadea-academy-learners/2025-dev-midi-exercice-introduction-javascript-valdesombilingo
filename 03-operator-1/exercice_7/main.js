95a3c26ae8a9e52102a736ce593935c34ebb5800
Michel Buhendwa <mkbuhendwa@gmail.com>
2025-07-02 15:00:05 +0100

    reset code de depart

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,8 +1,4 @@
-let ageEnfant=4;
-let agePere =ageEnfant *2;
-let  ageMere =agePere -5;
-let ageGrandPere =ageMere *2 + ageEnfant / 2;
-let ageOncle =agePere + 10;
+let ageEnfant, agePere, ageMere, ageGrandPere, ageOncle;
 // Ecrivez votre code ici
 
 // Ne pas modifier le code ci-dessous
--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,43 +1,5 @@
 let paul, marie, eric, clair, madameMukuna, joseph, sarah;
-let maison = 60000000;
-let terrains = 40000000;
-let liquidités = 20000000; 
-let heritage = maison + terrains + liquidités;
-// let trentepourcent = heritage*25/100;
-let douzepointcinqpourcent = vingtcinqpourcentpourcent/2
-let vingtcinqpourcent = heritage*25/100;
-paul = vingtcinqpourcentpourcent;
-marie = vingtcinqpourcentpourcent;
-madameMukuna = vingtcinqpourcent/3;
-joseph = vingtcinqpourcent/3;
-sarah = vingtcinqpourcent/3
-
-//le 75% de l'heritage est 90,000,000 CDF
-//les enfants ont à chacun 30,000,000 CDF
-eric = douzepointcinqpourcent;
-clair = douzepointcinqpourcent;
 // Ecrivez votre code ici
-let prisMaison=60_000_000;
-let terraisEstime = 40_000_000;
-let liquite =20_000_000;
-let totalHeritage=prisMaison + terraisEstime + liquite;
-
-let premiereCatgorie = totalHeritage*0.75;
-let partChaqueEnfant=premiereCatgorie/3;
-let ericEtClair =partChaqueEnfant/2;
-let deuxiemeCategorie= totalHeritage *0.25;
-let partConjointe = deuxiemeCategorie /3 ;
-
-paul = partChaqueEnfant,
-marie= partChaqueEnfant,
-eric =ericEtClair,
-clair = ericEtClair,
-madameMukuna = partConjointe,
-joseph = partConjointe,
-sarah = partConjointe;
-
-
-
 
 // Ne pas modifier le code ci-dessous
 module.exports = {
